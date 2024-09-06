4ffd5416b9d50e95580cb2dbf2f2ea0c940be7b0
rajan <rpoudel@sagetechpartners.com>
2024-09-06 16:08:41 +0545

    rajan design changes final with responsive

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -58,10 +58,10 @@ export default function Header({
       >
         <DialogBackdrop className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="flex  items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <DialogPanel
               transition
-              className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-sm sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+              className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-10 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-sm sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
             >
               <div>
                 <div className="mt-3 text-center sm:mt-5">
@@ -130,7 +130,7 @@ export default function Header({
 
       <header className="bg-white shadow">
         <div className="container mx-auto px-4 py-2 flex items-center justify-between">
-          <Link className="flex w-auto h-16 sm:h-20  py-2" to="/">
+          <Link className="flex w-48 h-16 sm:h-20 py-2 items-start" to="/">
             <img src={Logo} alt="BarCamp Kathmandu 2024" />
           </Link>
 
